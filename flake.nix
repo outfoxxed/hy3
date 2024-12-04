e1b001a9a1e38189d77c73f77f906b8164d00a90
outfoxxed <outfoxxed@outfoxxed.me>
2024-12-04 13:11:55 -0800

    nix: update lockfile

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?rev=a425fbebe4cf4238e48a42f724ef2208959d66cf";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland";
   };
 
   outputs = { self, hyprland, ... }: let
