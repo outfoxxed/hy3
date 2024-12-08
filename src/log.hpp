6763aa56e0a5bc19d3f32dd48d4016fbf9736bb4
myamusashi <jinxprogilang666@gmail.com>
2024-12-08 11:17:11 +0700

    Rename LogLevel to eLogLevel

--- src/log.hpp
+++ src/log.hpp
@@ -3,7 +3,7 @@
 #include <hyprland/src/debug/Log.hpp>
 
 template <typename... Args>
-void hy3_log(LogLevel level, std::format_string<Args...> fmt, Args&&... args) {
+void hy3_log(eLogLevel level, std::format_string<Args...> fmt, Args&&... args) {
 	auto msg = std::vformat(fmt.get(), std::make_format_args(args...));
 	Debug::log(level, "[hy3] {}", msg);
 }
