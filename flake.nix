a9caea020e457b4c5868913eea391beffd5fc22c
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-05-05 18:30:31 +0200

    fixup: apply renaming changes
    Members for various classes got m_ prefix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,50 @@
 {
   inputs = {
-    hyprland.url = "github:hyprwm/Hyprland?rev=72cb5d24b6894334da021157c626556a8e99b6ff";
+    hyprland.url = "github:hyprwm/Hyprland?rev=1ce614dfc0eb8b323e603b76975842c1f2e6a553";
   };
 
-  outputs = { self, hyprland, ... }: let
-    inherit (hyprland.inputs) nixpkgs;
+  outputs =
+    { self, hyprland, ... }:
+    let
+      inherit (hyprland.inputs) nixpkgs;
 
-    hyprlandSystems = fn: nixpkgs.lib.genAttrs
-      (builtins.attrNames hyprland.packages)
-      (system: fn system nixpkgs.legacyPackages.${system});
+      hyprlandSystems =
+        fn:
+        nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (
+          system: fn system nixpkgs.legacyPackages.${system}
+        );
 
-    hyprlandVersion = nixpkgs.lib.removeSuffix "\n" (builtins.readFile "${hyprland}/VERSION");
-  in {
-    packages = hyprlandSystems (system: pkgs: rec {
-      hy3 = pkgs.callPackage ./default.nix {
-        hyprland = hyprland.packages.${system}.hyprland;
-        hlversion = hyprlandVersion;
-      };
-      default = hy3;
-    });
+      hyprlandVersion = nixpkgs.lib.removeSuffix "\n" (builtins.readFile "${hyprland}/VERSION");
+    in
+    {
+      packages = hyprlandSystems (
+        system: pkgs: rec {
+          hy3 = pkgs.callPackage ./default.nix {
+            hyprland = hyprland.packages.${system}.hyprland;
+            hlversion = hyprlandVersion;
+          };
+          default = hy3;
+        }
+      );
 
-    devShells = hyprlandSystems (system: pkgs: {
-      default = import ./shell.nix {
-        inherit pkgs;
-        hlversion = hyprlandVersion;
-        hyprland = hyprland.packages.${system}.hyprland-debug;
-      };
+      devShells = hyprlandSystems (
+        system: pkgs: {
+          default = import ./shell.nix {
+            inherit pkgs;
+            hlversion = hyprlandVersion;
+            hyprland = hyprland.packages.${system}.hyprland-debug;
+          };
 
-      impure = import ./shell.nix {
-        pkgs = import <nixpkgs> {};
-        hlversion = hyprlandVersion;
-        hyprland = (pkgs.appendOverlays [ hyprland.overlays.hyprland-packages ]).hyprland-debug.overrideAttrs {
-          dontStrip = true;
-        };
-      };
-    });
-  };
+          impure = import ./shell.nix {
+            pkgs = import <nixpkgs> { };
+            hlversion = hyprlandVersion;
+            hyprland =
+              (pkgs.appendOverlays [ hyprland.overlays.hyprland-packages ]).hyprland-debug.overrideAttrs
+                {
+                  dontStrip = true;
+                };
+          };
+        }
+      );
+    };
 }
