00d06a011163ea1e5d7107ad6d1d070f97c906e0
adinhodovic <hodovicadin@gmail.com>
2026-08-29 12:40:24 +0200

    fix: hyprpm version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    hyprland.url = "github:hyprwm/hyprland/36b2e0cfe0c6094dbc47bd42a437431315bb3087";
+    hyprland.url = "github:hyprwm/hyprland/5c9377c15f85c50648f35ca5a213754f95b93ca0";
   };
 
   outputs = {
